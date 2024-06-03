a04dd77cdb4c4c1e5c04eee9220395a6a3244bac
shikha <shikhashivi1111@gmail.com>
2024-06-03 17:39:06 +0530

    Adding changes

--- Client/src/Home.jsx
+++ Client/src/Home.jsx
@@ -5,21 +5,15 @@ const Home = () => {
     <>
       <div className='p-5'>
         <h1>Home hello</h1>
-        <p>You can store anyone details in this db</p>
+        <p>Welcome , your data is stored to the DATABASE</p>
       </div>
       <div>
         <div className='d-flex flex-direction-column'>
         <Link to="/login" className='btn btn-success border w-100  rounded-5 text-decoration-none'>
-            Login
+            Logout
           </Link>
         </div>
-        <div>
-        <Link to="/register" className='btn btn-success border w-100  rounded-5 text-decoration-none'>
-            Register
-          </Link>
-
        
-        </div>
       </div>
     </>
   )
