3ccdab937427a0e67cf5b0eda4d60ea4c6c2dbed
shikha <shikhashivi1111@gmail.com>
2024-06-05 07:43:44 +0530

    Router change

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
    <>
   <BrowserRouter>
   <Routes>
-    <Route path="/register" element={<Signup/>}/>
+    <Route path="/" element={<Signup/>}/>
     <Route path="/login" element={<Login/>}/>
     <Route path="/home" element={<Home/>}/>
 
